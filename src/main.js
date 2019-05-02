b7c2abdfa5bf7ec9ac1457b1ab8438f98a38655c
shideshanxx <307042873@qq.com>
2019-05-02 12:09:00 +0800

    完善

--- src/main.js
+++ src/main.js
@@ -8,10 +8,16 @@ import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
+/* eslint-disable no-unused-vars */
+// import vConsole from 'vconsole'
+// vConsole是一个手机调试工具
+
+console.log('test')
+
 fastclick.attach(document.body)
 // 下面这句话的意思是阻止启动生产消息
 Vue.config.productionTip = false
-// 使用懒加载
+// 使用图片懒加载
 Vue.use(VueLazyLoad, {
   loading: require('common/image/default.png')
 })
