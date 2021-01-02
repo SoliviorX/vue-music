3c0ee58f7025d69ddfece5abb4bfeab9118a7faa
石德山 <307042873@qq.com>
2021-01-02 21:43:58 +0800

    歌手页面

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -5,3 +5,12 @@ export function addClass (el, className) {
 export function hasClass (el, className) {
   return el.classList.contains(className)
 }
+
+// 获取或设置data-.. 的值
+export function getData(el, name, val) {
+  const prefix = 'data-'
+  if (val) {
+    return el.setAttribute(prefix + name, val)
+  }
+  return el.getAttribute(prefix + name)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,98 @@
 <template>
-  <div>singer</div>
+  <div class="singer">
+    <list-view :data='singers'></list-view>
+  </div>
 </template>
 
 <script>
-export default {
+import { getSingerList } from "api/singer";
+import { ERR_OK } from "api/config";
+import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
 
-}
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+export default {
+  data() {
+    return {
+      singers: [],
+    };
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list);
+          console.log(this.singers)
+        }
+      });
+    },
+    // 数据处理
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: [],
+        },
+      };
+      list.forEach((item, index) => {
+        // 取前十条数据为 热门
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid,
+            })
+          );
+        }
+        const key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: [],
+          };
+        }
+        map[key].items.push(
+          new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid,
+          })
+        );
+      });
+      // 为了得到有序列表，我们需要处理 map
+      let ret = [];
+      let hot = [];
+      // 遍历对象
+      for (let key in map) {
+        let val = map[key];
+        // 把对象分成两个数组
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val);
+        } else if (val.title === HOT_NAME) {
+          hot.push(val);
+        }
+      }
+      // 对数组按a-z排序
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+      });
+      return hot.concat(ret);
+    },
+  },
+  components: {
+    ListView
+  }
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
